c7171d2736ce150dacc23a32bde813eb64a29efa
Alexander Billington <alexander@advancinganalytics.co.uk>
2023-02-27 14:11:24 +0000

    Change error logging statements to logging.error()

--- Basic/__init__.py
+++ Basic/__init__.py
@@ -25,14 +25,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         json_msg = req.get_json()
         logging.info(f"Json message recieved: {json_msg}")
     except ValueError:
-        logging.info("No json message recieved.")
+        logging.error("No json message recieved.")
         return return_error("No json message recieved")
 
     # Load the model
     try:
         model = pickle.load(open(f"{os.getcwd()}/models/Random Forest.pkl", 'rb'))
     except Exception as e:
-        logging.info(f"Failed to load model, {e}")
+        logging.error(f"Failed to load model, {e}")
         return return_error("Failed to load model")
 
     # Preprocess data for the model
@@ -41,7 +41,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         model_inpt = np.array(list(json_msg.values())).reshape(1,-1)
         logging.info(f"Model input: {model_inpt}")
     except Exception as e:
-        logging.info(f"Failed preparing data for model, {e}")
+        logging.error(f"Failed preparing data for model, {e}")
         return return_error("Failed preparing data for model")
 
     # Query the model
@@ -49,7 +49,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         prediction = model.predict(model_inpt)
         logging.info(f"Prediction: {prediction}")
     except Exception as e:
-        logging.info(f"Failed to query model, {e}")
+        logging.error(f"Failed to query model, {e}")
         return return_error("Failed to query model")
 
     return func.HttpResponse(f"{prediction}",
