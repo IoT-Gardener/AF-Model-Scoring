cee2fced8a13342ed31052bfb772a49c227ade5d
Alexander Billington <alexander@advancinganalytics.co.uk>
2023-02-27 11:09:53 +0000

    Add functionality for model scoring

--- Basic/__init__.py
+++ Basic/__init__.py
@@ -1,24 +1,56 @@
+import json
 import logging
-
+import os
+import pickle
 import azure.functions as func
+import numpy as np
 
+def return_error(msg: str) -> func.HttpResponse:
+    """Function to create http response for any given error
+
+    :param msg: The message to add to the response
+    :type msg: str
+    :return: Returns a HttpResponse for the relevant issue
+    :rtype: func.HttpResponse
+    """
+    return func.HttpResponse(body=f"{msg}", 
+                             headers={"Content-Type": "application/json"},
+                             status_code=500)
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    # Read the message body
+    try:
+        json_msg = req.get_json()
+        logging.info(f"Json message recieved: {json_msg}")
+    except ValueError:
+        logging.info("No json message recieved.")
+        return return_error("No json message recieved")
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    # Load the model
+    try:
+        model = pickle.load(open(f"{os.getcwd()}/models/Random Forest.pkl", 'rb'))
+    except Exception as e:
+        logging.info(f"Failed to load model, {e}")
+        return return_error("Failed to load model")
+
+    # Preprocess data for the model
+    try:
+        # Convert the input dict to a numpy array compatible with the model
+        model_inpt = np.array(list(json_msg.values())).reshape(1,-1)
+        logging.info(f"Model input: {model_inpt}")
+    except Exception as e:
+        logging.info(f"Failed preparing data for model, {e}")
+        return return_error("Failed preparing data for model")
+
+    # Query the model
+    try:
+        prediction = model.predict(model_inpt)
+        logging.info(f"Prediction: {prediction}")
+    except Exception as e:
+        logging.info(f"Failed to query model, {e}")
+        return return_error("Failed to query model")
+
+    return func.HttpResponse(f"{prediction}",
+                             status_code=200)
